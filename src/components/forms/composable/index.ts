27ca21dd12c98d15ba7160838c7236cdd950323d
Krzysztof Bzowski <kbzowski@agh.edu.pl>
2026-06-12 23:35:08 +0200

    chore: remove unused form barrel files
    composable/ and submission/ index barrels: no importers; consumers import per-file.

--- src/components/forms/composable/index.ts
+++
@@ -1,7 +0,0 @@
-export { FormCheckboxField } from "./checkbox-field";
-export { FormIconInputField } from "./icon-input-field";
-export { FormInputField } from "./input-field";
-export { FormPasswordField } from "./password-field";
-export { FormSelectField } from "./select-field";
-export { FormSwitchField } from "./switch-field";
-export { FormTextareaField } from "./textarea-field";
--- src/components/forms/submission/index.ts
+++
@@ -1,4 +0,0 @@
-export { type Author, AuthorsInput } from "./authors-input";
-export { FileDropzone } from "./file-dropzone";
-export { KeywordsInput } from "./keywords-input";
-export { SubmissionForm, type SubmissionFormData } from "./submission-form";
