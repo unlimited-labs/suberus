3b188fac0f0de7d6fcc5995e987b192cd7ee8736
Krzysztof Bzowski <kbzowski@agh.edu.pl>
2026-06-12 15:28:06 +0200

    feat: new favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -73,11 +73,30 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 			},
 			{
 				rel: "icon",
-				href: "/favicon.png",
+				type: "image/png",
+				href: "/favicon-96x96.png",
+				sizes: "96x96",
+			},
+			{
+				rel: "icon",
+				type: "image/svg+xml",
+				href: "/favicon.svg",
+			},
+			{
+				rel: "shortcut icon",
+				href: "/favicon.ico",
+			},
+			{
+				rel: "apple-touch-icon",
+				sizes: "180x180",
+				href: "/apple-touch-icon.png",
+			},
+			{
+				rel: "manifest",
+				href: "/site.webmanifest",
 			},
 		],
 	}),
-
 	component: RootComponent,
 	shellComponent: RootDocument,
 	notFoundComponent: NotFoundPage,
